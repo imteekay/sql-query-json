0cd09c4cf5188af50e0471942c59ef93ac7d15e1
TK <leandrotk100@gmail.com>
2019-08-11 15:33:01 -0300

    Change from filter-map to a single reduce

--- 001.js
+++ 001.js
@@ -35,16 +35,16 @@ where(attribute, attributeValue)
 
 const startEngine = (json) => (attributes) => ({ from: from(json, attributes) });
 
-const byAttributeValue = (attribute, value) => (node) => node[attribute] === value;
-
 const buildAttributes = (node) => (acc, attribute) => ({ ...acc, [attribute]: node[attribute] });
 
-const selectAttributes = (attributes) => (node) => attributes.reduce(buildAttributes(node), {});
+const executeQuery = (attributes, attribute, value) => (resultList, node) =>
+  node[attribute] !== value
+    ? resultList
+    : resultList.concat(attributes.reduce(buildAttributes(node), {}));
 
 const where = (json, attributes) => (attribute, value) =>
   json
-    .filter(byAttributeValue(attribute, value))
-    .map(selectAttributes(attributes));
+    .reduce(executeQuery(attributes, attribute, value), []);
 
 const from = (json, attributes) => (node) => ({ where: where(json[node], attributes) });
 
